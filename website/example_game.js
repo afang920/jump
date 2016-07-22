a66f7b5c7531c7df590348e44270169e6bf54fe5
afang920 <afang92@gmail.com>
2016-07-22 10:39:36 -0700

    rearranging / sorting

---
+++ website/example_game.js
@@ -0,0 +1,9 @@
+function preload() {
+
+    game.load.baseURL = 'http://examples.phaser.io/assets/';
+    game.load.crossOrigin = 'anonymous';
+
+    game.load.image('phaser', 'sprites/phaser-dude.png');
+
+}
+
